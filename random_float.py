b59406c79dacbbbdcd76be4e32a34d5eabec62e0
furtunb <47780420+furtunb@users.noreply.github.com>
2020-09-26 16:38:27 +0300

    Update random_float.py

--- random_float.py
+++ random_float.py
@@ -7,9 +7,9 @@ def random_float(integer_min=0, integer_max=None , min_decimal=0, max_decimal=No
     min_decimal: minimum decimal number for float side. by default it is 0
     max_decimal: maximum decimal number for float side. by default it is min_decimal + 1
     """
-    if (max_decimal == None) or (min_decimal > max_decimal):
+    if (max_decimal is None) or (min_decimal > max_decimal):
         max_decimal = min_decimal+1
-    if (integer_max == None) or (integer_min > integer_max):
+    if (integer_max is None) or (integer_min > integer_max):
         integer_max = integer_min + 1
     integer = random.randint(integer_min, integer_max)
     floatSide = (random.randint(10**min_decimal, 10**max_decimal))*(10**(-max_decimal))
